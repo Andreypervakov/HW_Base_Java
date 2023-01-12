51e30333f444560435366f59949cc8565282e913
Andrey Pervakov <paafox907@gmail.com>
2023-01-12 08:16:07 +0300

    added format strings

--- Same_Number.java
+++ Same_Number.java
@@ -5,13 +5,20 @@ public class Same_Number {
         int d = number / 10;
         int e = number % 10;
 
-        int sum = d+e;
-        int mulip = d*e;
+        //int sum = d+e;
+        //int mulip = d*e;
 
-        System.out.println(d);
-        System.out.println(e);
-        System.out.println(sum);
-        System.out.println(mulip);
+        //System.out.println(d);
+        //System.out.println(e);
+        //System.out.println(sum);
+        //System.out.println(mulip);
+        //
+        //
+        System.out.printf("In this number %d - %d tens. \n", number, d);
+        System.out.printf("In this number %d - %d ones. \n", number, e);
+        System.out.printf("Sum of this number %d - %d. \n", number, d + e);
+        System.out.printf("Multiplication of this number %d - %d. \n \n", number, d * e);
     }
 }
+
 
