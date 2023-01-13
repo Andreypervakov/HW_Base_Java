6b1fb569c0e6424cbc75f5f8746672cf73365217
Andrey Pervakov <paafox907@gmail.com>
2023-01-13 16:27:59 +0300

    ver 0.1

---
+++ Lesson_2/ForCycalPrinted.java
@@ -0,0 +1,9 @@
+public class ForCycalPrinted {
+    public static void main(String[] args) {
+        int i = 10;
+        for (i =10; i < 26; i++){
+            System.out.println(i + " " + i +".4");
+        }
+    }
+}
+
