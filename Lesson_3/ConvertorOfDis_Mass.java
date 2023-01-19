aaf32b78e432a98a6e2c1eea90c2a2785f2ebf7d
Andrey Pervakov <paafox907@gmail.com>
2023-01-19 15:30:23 +0300

    it is not ended that just a thoughts

---
+++ Lesson_3/ConvertorOfDis_Mass.java
@@ -0,0 +1,81 @@
+public class ConvertorOfDis_Mass
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Выбор конвертации :");
+        String numOne = "1";
+        String numTwo = "2";
+    }
+    chooseFist(numOne, numTwo);
+    public static void chooseFist(String num)
+    {
+        boolean numFirst = false;
+        boolean numSecond = true;
+        return ();
+
+    }
+    public static void chooseSecondary()
+    {   byte a = 1;
+        byte b = 2;
+        byte c = 3;
+        byte d = 4;
+    }
+    public static void massConver ()
+    {switch (num)
+    {
+        case 1 -> System.out.println("Считаем килограммы. " + weight + " - это: \n" +
+                "Килограммы: " + weight + "\n" +
+                "Граммы:" + weight * 1000 + "\n" +
+                "Фунты: " + weight * 2.2046226218487757 + "\n" +
+                "Караты: " + weight * 5000);
+        case 2 -> System.out.println("Считаем граммы. " + weight + " - это: \n" +
+                "Килограммы: " + weight / 1000 + "\n" +
+                "Граммы: " + weight + "\n" +
+                "Фунты: " + weight * 0.002204622621848776 + "\n" +
+                "Катары: " + weight * 5);
+        case 3 -> System.out.println("Считаем фунты. " + weight + " - это: \n" +
+                "Килограммы: : " + weight * 0.45359237 + "\n" +
+                "Граммы: " + weight * 453.59237 + "\n" +
+                "Фунты: " + weight + "\n" +
+                "Караты: " + weight * 2267.96185);
+        case 4 -> System.out.println("Считаем караты. " + weight + " - это: \n" +
+                "Килограммы: " + weight / 5000 + "\n" +
+                "Граммы: " + weight * 0.2 + "\n" +
+                "Фунты: " + weight / 2267.96185 + "\n" +
+                "Караты: " + weight);
+    }
+
+    }
+
+
+
+    }
+    public  static void distConver()
+    {switch ()
+    {    case 1 -> System.out.println("Считаем метры. " + dist + " - это: \n" +
+            "Метры: " + dist + "\n" +
+            "Мили:" + dist / 1609.347 + "\n" +
+            "Ярды: " + dist * 1.0936132983377078 + "\n" +
+            "Футы: " + dist * 3.280839895013123);
+        case 2 -> System.out.println("Считаем мили. " + dist + " - это: \n" +
+                "Метры: " + dist * 1609.347 + "\n" +
+                "Мили:" + dist + "\n" +
+                "Ярды: " + dist * 1760.003280839895 + "\n" +
+                "Футы: " + dist * 5280.009842519685);
+        case 3 -> System.out.println("Считаем ярды. " + dist + " - это: \n" +
+                "Метры: " + dist / 1.0936132983377078 + "\n" +
+                "Мили:" + dist / 1760.003280839895 + "\n" +
+                "Ярды: " + dist + "\n" +
+                "Футы: " + dist * 3);
+        case 4 -> System.out.println("Считаем футы. " + dist + " - это: \n" +
+                "Метры: " + dist * 0.3048 + "\n" +
+                "Мили:" + dist / 5280.009842519685 + "\n" +
+                "Ярды: " + dist / 3 + "\n" +
+                "Футы: " + dist);
+
+    }
+
+
+}
+}
+
